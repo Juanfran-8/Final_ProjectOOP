fc4b3d06bcdbfe8cc81cb46e6071285d268cae08
Juanfran-8 <juanfranciscoc833@gmail.com>
2026-04-09 20:59:03 -0500

    Update DeliverySystem.cs

--- Final_ProjectOOP/DeliverySystem.cs
+++ Final_ProjectOOP/DeliverySystem.cs
@@ -8,5 +8,65 @@ namespace Final_ProjectOOP
 {
     public class DeliverySystem
     {
+        List <Warehouse> warehouses;
+List <Package> allPackages;
+
+public void AddWarehouse(Warehouse w)
+{
+    warehouses.Add(w);
+}
+
+public void AddPackage(Package p)
+{
+        allPackages.Add(p);
+}
+
+Package SearchPackageById(int id)
+{
+    foreach (Package p in allPackages)
+    {
+        if (p.GetId() == id)
+        {
+            return p;
+        }
+    }
+    return null;
+}
+
+public void SortPackages(List <Package> list)
+{
+    for (int i = 0; i < list.Count - 1; i++)
+    {
+        for (int j = 0; j < list.Count - i - 1; j++)
+        {
+            if (list[j].CalculatePriorityScore() > list[j + 1].CalculatePriorityScore())
+            {
+                var temp = list[j];
+                list[j] = list[j + 1];
+                list[j + 1] = temp;
+            }
+        }
+
+    }
+
+}
+public void ProcessDelivery() // finalizar este método para processar el envio 
+{
+    foreach (var w  in warehouses)
+    {
+        
+    }
+
+}
+
+public void SimulateDay()
+{
+    Console.WriteLine("== Simulation of a day in the delivery system ==");
+    ProcessDelivery();
+    Console.WriteLine("\n==== Simulation end ====");
+
+
+
+}
     }
 }
